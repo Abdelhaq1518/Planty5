297fe939fc78bde11971a8c99517a8728e8902c3
Abdel Ait Said <aaitsaid78@gmail.com>
2023-10-31 13:40:53 +0100

    Ajout de l'ensemble du site aux fichiers du thème enfant

---
+++ wordpress/wp-content/themes/index.php
@@ -0,0 +1,25 @@
+<div class="compteur-quantites">
+    <div class="valeur">
+        <input type="text" value="0" class="compteur-value" name="compteur">
+    </div>
+    <div class="bouton">
+        <button class="bouton-compteur" data-action="increment">+</button>
+        <button class="bouton-compteur" data-action="decrement">-</button>
+    </div>
+    <div class="valeur">
+        <input type="text" value="0" class="compteur-value" name="compteur">
+    </div>
+    <div class="bouton">
+        <button class="bouton-compteur" data-action="increment">+</button>
+        <button class="bouton-compteur" data-action="decrement">-</button>
+    </div>
+    <button class="bouton-valider">Ok</button>
+    <div class="valeur">
+        <input type="text" value="0" class="compteur-value" name="compteur">
+    </div>
+    <div class="bouton">
+        <button class="bouton-compteur" data-action="increment">+</button>
+        <button class="bouton-compteur" data-action="decrement">-</button>
+    </div>
+    <button class="bouton-valider">Ok</button>
+</div>
